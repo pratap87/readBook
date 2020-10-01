4d8c26511d379d1165a188e2f001629dc941ab00
Arun <apschauhan60@gmail.com>
2020-10-01 11:48:15 +0530

    bookApp functionality completed

---
+++ build/precache-manifest.28cee8d7cfe9b958966dbb9e4a030f1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f4079adc98496499854",
+    "url": "/static/js/main.0f4079ad.chunk.js"
+  },
+  {
+    "revision": "e6dcca54ff7dd7f37e60",
+    "url": "/static/js/1.e6dcca54.chunk.js"
+  },
+  {
+    "revision": "0f4079adc98496499854",
+    "url": "/static/css/main.f66cd613.chunk.css"
+  },
+  {
+    "revision": "6995c8155c83e5346893cf949879621f",
+    "url": "/index.html"
+  }
+];
---
+++ src/a.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Main from './Main'
+import Search from './search'
+
+
+
+class BooksApp extends React.Component {
+    state = {
+        showSearchPage: false,
+        books: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll()
+            .then((books) => {
+                //console.log(books);
+                this.setState(() => ({
+                    books
+                }))
+            })
+    }
+
+    updateShelf = (book, shelf) => {
+
+        BooksAPI.update(book, shelf)
+            .then((result) => {
+                //update frontend once book is updated from API
+                book.shelf = shelf
+                this.setState((currentState) => ({
+                    books: currentState.books.filter((x) => {
+                        return x.id !== book.id
+                    }).concat([book])
+                }))
+
+            })
+    }
+    render() {
+        return (
+            <div>
+                <Router>
+                    <div>
+                        <Route exact path='/' render={() => (
+                            <Main
+                                books={this.state.books}
+                                onUpdateShelf={this.updateShelf}
+                            />
+                        )} />
+                        <Route path='/search' render={() => (
+                            <Search
+                                books={this.state.books}
+                                onUpdateShelf={this.updateShelf}
+                            />
+                        )} />
+                    </div>
+                </Router>
+            </div>
+        )
+    }
+}
+
+export default BooksApp
